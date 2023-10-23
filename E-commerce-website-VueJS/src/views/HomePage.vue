31e4dd7a175513f84e6299119864a1451d53d56a
Arafat Hossain Ar <arafat.122260@gmail.com>
2023-10-23 13:20:20 +0600

    feat: create HomePage.vue

---
+++ E-commerce-website-VueJS/src/views/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+        <HomeCarosual />
+        <HomeFeatures />
+        <HomeFeaturedProducts/>
+        <HomeLatestProducts/>
+        <HomeSwiper/>
+        <HomeTestimonials/>
+</template>
+<script>
+import HomeCarosual from '../components/HomeCarosual.vue';
+import HomeFeatures from '../components/HomeFeatures.vue';
+import HomeFeaturedProducts from '../components/HomeFeaturedProducts.vue';
+import HomeLatestProducts from '../components/HomeLatestProducts.vue';
+import HomeTestimonials from '../components/HomeTestimonials.vue';
+import HomeSwiper from '../components/HomeSwiper.vue';
+export default {
+    name: 'HomePage',
+    components: {
+        HomeCarosual,
+        HomeFeatures,
+        HomeFeaturedProducts,
+        HomeLatestProducts,
+        HomeTestimonials,
+        HomeSwiper
+    }
+}
+</script>
