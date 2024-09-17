12b03735cc4d3f93141dfa40b199794bbac69520
Vinayak-Sannaik <vinayakpsannaik@gmail.com>
2024-09-17 12:28:21 +0530

    React js Mini Basic Projects

---
+++ Mini Project/src/dummyData.js
@@ -0,0 +1,6 @@
+const Data = [
+    {id : 1, title : 'Task one', completed : false },
+    { id: 2, title: 'Task Two', completed: true },
+    { id: 3, title: 'Task Three', completed: false },
+]; 
+export default Data;
